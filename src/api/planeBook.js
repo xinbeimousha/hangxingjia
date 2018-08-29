e92a7f1e40cb605171c49c16265af84a8dfccb4d
xinbeimousha <m18675931249@163.com>
2018-08-29 17:20:58 +0800

    写好注释

--- src/api/planeBook.js
+++ src/api/planeBook.js
@@ -1,10 +1,12 @@
 import request from './config.js';
-
-export function getBudgetSpaceType(){
-     return request({
-         url:'appBudgetRuleController.do?getBudgetSpaceType',
-         method:'post'
-     }).then(res => {
-         return Promise.resolve(res.data)
-     })             
+/**
+ * @description 获取舱位等级接口
+ */
+export function getBudgetSpaceType() {
+  return request({
+    url: 'appBudgetRuleController.do?getBudgetSpaceType',
+    method: 'post'
+  }).then(res => {
+    return Promise.resolve(res.data)
+  })
 }
