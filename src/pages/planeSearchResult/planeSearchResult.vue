7f63b80af12b10b7f07161d250b1aeb3780026e0
xinbeimousha <m18675931249@163.com>
2018-09-03 17:25:40 +0800

    新建查询结果页面

---
+++ src/pages/planeSearchResult/planeSearchResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search-result">
+    查询结果
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+  .search-result{
+    
+  }
+</style>
+
+
