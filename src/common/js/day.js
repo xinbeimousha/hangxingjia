0a912791f81b5b4ad723ba812ec7a188b337dfa1
wangchuan <vuexjwangchuan@163.com>
2018-09-06 17:43:58 +0800

    dayjs

--- src/common/js/day.js
+++ src/common/js/day.js
@@ -15,15 +15,12 @@ export function getDate2(time){
  * @description 获取时间,格式hh:mm
  */
 export function getTime(time){
-<<<<<<< HEAD
-  return day(time).format('hh:ss');
+  return day(time).format('hh:mm');
 }
 /**
  * @description 获取日期,格式mm/ss
  */
 export function getDate3(time){
   return day(time).format('MM-DD');
-=======
-  return day(time).format('hh:mm');
->>>>>>> 3ca790ed14d0f71198781334e7c5dd0410db8ffe
+  
 }
