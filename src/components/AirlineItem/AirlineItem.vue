21ffc5af3df83aa2d220440d1108a8fe2bcac95c
xinbeimousha <m18675931249@163.com>
2018-09-13 17:05:29 +0800

    完成机票订单页面

---
+++ src/components/AirlineItem/AirlineItem.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="airline">
+    <div class="content">
+      <div class="time">
+        <div class="depart">08:00</div>
+        <div class="line"></div>
+        <div class="arrival">11:10</div>
+      </div>
+      <div class="airport">
+        <div class="from">广州白云机场T2</div>
+        <div class="to">北京首都国际机场T2</div>
+      </div>
+      <div class="air-info">
+        <img :src="require(`common/img/logo/2Z.png`)" class="icon">
+        <span class="carrier">CZ2301</span>
+        <span class="date">2018/08/28</span>
+        <span class="share">共享 CZ2001</span>
+      </div>
+    </div>
+    <div class="price">
+      ¥1130
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/style/variable.styl';
+@import '~common/style/mixin.styl';
+
+.airline {
+  x-middle();
+  justify-content: space-between;
+  padding: 0.2rem 0;
+
+  .content {
+    flex: 1;
+
+    .time {
+      x-middle();
+      justify-content: space-between;
+
+      .line {
+        position: relative;
+        width: 1.8rem;
+        border-bottom: 1px solid #979797;
+
+        &:before {
+          content: '';
+          display: block;
+          position: absolute;
+          left: 0;
+          width: 5px;
+          height: 5px;
+          border-radius: 50%;
+          background-color: #d8d8d8;
+          transform: translate(-50%, -50%);
+        }
+
+        &:after {
+          content: '';
+          display: block;
+          position: absolute;
+          right: 0;
+          width: 5px;
+          height: 5px;
+          border-radius: 50%;
+          background-color: #d8d8d8;
+          transform: translate(50%, -50%);
+        }
+      }
+    }
+
+    .airport {
+      display:flex;
+      justify-content: space-between;
+      padding:0.1rem 0;
+      font-size: 0.24rem;
+      color: $color-text-h;
+
+      .from,.to{
+        width:35%;
+      }
+      .to{
+        text-align :right;
+      }
+    }
+
+    .air-info{
+      font-size:0.24rem;
+      color: $color-text-h;
+
+      span{
+        vertical-align:bottom;
+      }
+      .icon{
+        display :inline-block;
+        width:0.5rem;
+        vertical-align:middle;
+      }
+      .carrier{
+        margin-right:0.1rem;
+      }
+      .date,.share{
+        border-left:2px solid $color-solid;
+        padding-left:0.2rem;
+      }
+      .date{
+        padding-right:0.1rem;
+      }
+    }
+  }
+
+  .price {
+    align-self: flex-start;
+    width: 1.7rem;
+    color:$color-text-active;
+    text-align: right;
+  }
+}
+</style>
+
+
--- src/pages/DomeOrder/DomeOrder.vue
+++ src/pages/DomeOrder/DomeOrder.vue
@@ -4,19 +4,75 @@
       title="机票订单"
       :btnLeft="true"
     />
-    <div class="dome-order-content"></div>
+    <div class="dome-order-content">
+      <div class="airline-wrapper" v-if="airlineData.length > 0">
+        <AirLine />
+      </div>
+      <div class="passenger">
+        <div class="people">
+          <div class="title">
+            乘机人
+          </div>
+          <div class="names">
+            <span class="name">唐文</span>
+          </div>
+          <div class="add">
+            添加乘机人
+          </div>
+        </div>
+        <div class="phone">
+          <div class="title">
+            手机
+          </div>
+          <div class="num">
+            <input type="text" v-model="telephone" placeholder="输入电话号码" name="" id="">
+          </div>
+        </div>
+      </div>
+      <div class="retreating">
+        <p class="title">退改声明</p>
+        <p class="content">航班预计离站时间前2小时航班预计离站时间前2小时航班预计离站时间前2小时航班预计离站时间前2小时航班预计离站时间前2小时</p>
+      </div>
+    </div>
     <div class="dome-order-footer">
-      
+      <span class="total-price">
+        总额 ¥ 2930
+      </span>
+      <span class="submit-order">
+        提交订单
+      </span>
     </div>
   </div>
 </template>
 
 <script>
 import HeaderTitle from 'components/HeaderTitle/HeaderTitle';
+import AirLine from 'components/AirlineItem/AirlineItem';
+import { getLocal } from 'common/js/storage.js';
 
 export default {
   components:{
-    HeaderTitle
+    HeaderTitle,
+    AirLine
+  },
+  created(){
+    this._getAirlineData();
+  },
+  data(){
+    return {
+      telephone:18675961249,
+      airlineData:[]
+    }
+  },
+  methods:{
+    _getAirlineData(){
+      const airlineData = JSON.parse(getLocal('airlines'));
+      if(airlineData.length){
+        this.airlineData = airlineData;
+      }
+      console.log(this.airlineData)
+
+    }
   }
 }
 </script>
@@ -30,6 +86,65 @@ export default {
 
   .dome-order-content{
     flex:1;
+
+    .airline-wrapper{
+      padding:0 0.2rem;
+      margin-bottom:0.1rem;
+      background-color :#fff;
+    }
+    .passenger{
+      padding:0.2rem;
+      background-color :#fff;
+      
+      .title{
+          width:2rem;
+        }
+      .people{
+        x-middle();
+        .names{
+          flex:1;
+          color:$color-text;
+        }
+        .add{
+          color:$color-text-active;
+        }
+      }
+      .phone{
+        x-middle();
+        margin-top:0.2rem;
+
+        .num{
+          color:$color-text;
+        }
+      }
+    }
+    .retreating{
+      padding:0.2rem;
+      font-size:12px;
+
+      .title{
+        color:$color-text-active;
+      }
+      .content{
+        color:$color-text-h;
+        margin-bottom:0.1rem;
+      }
+    }
+  }
+  .dome-order-footer{
+    x-middle();
+    padding-left:0.2rem;
+    background-color:#fff;
+
+    .total-price{
+      flex:1;
+      color:$color-text-h;
+    }
+    .submit-order{
+      padding:0.2rem 0.3rem;
+      color:#fff;
+      background-color:$color-text-active;
+    }
   }
 }
 </style>
